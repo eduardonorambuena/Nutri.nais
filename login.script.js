b34b4a25a8dd65ca7571c17364fb7fa02368f275
chenaoh <cdhenao@gmail.com>
2024-04-21 18:30:27 -0400

    subida

--- login.script.js
+++ login.script.js
@@ -6,7 +6,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     if (username === 'admin' && password === 'admin123') {
         alert('Inicio de sesión exitoso');
         // Redirigir a la página index.html
-        window.location.href = 'index.html';
+        window.location.href = 'principal.html';
     } else {
         alert('Usuario o contraseña incorrectos');
     }
